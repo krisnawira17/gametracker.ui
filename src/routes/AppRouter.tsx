38304e9a807008b005e975ecfc0674524207eb0d
Krisna Wira Indrawan <krisnawiraindrawan@gmail.com>
2025-06-09 17:17:56 +0700

    init

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
+}
+
+export default AppRouter;
